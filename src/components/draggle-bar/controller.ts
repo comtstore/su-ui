c63f0f4180b673ae23e64ab6082c28404596ee45
solingjees <1600346867@qq.com>
2023-08-11 14:17:37 +0800

    feat: draggleBar支持group群组触发

--- src/components/draggle-bar/controller.ts
+++ src/components/draggle-bar/controller.ts
@@ -1,24 +1,57 @@
+/* eslint-disable no-unused-vars */
 import { DraggleBarProps } from "./interface";
 import { baseController} from '../../common/common.interface'
 import { action, makeObservable, observable } from "mobx";
+
+const groups: Map<string, Set<DraggleBarController>> = new Map()
 
 class DraggleBarController extends baseController<DraggleBarProps>{
     public constructor(props){
         super(props)
         makeObservable(this)
+        this.initialState()
+    }
+
+    public initialState = () => {
+        this.initialGroup()
+    }
+
+    private initialGroup = () => {
+        const groupId = this.props.group
+        if(groupId){
+            let group = groups.get(groupId)
+            if(!group){
+                group = new Set()
+                groups.set(groupId, group)
+            }
+            group.add(this)
+        }
     }
 
     @observable
     public isDragging: boolean = false
 
+    private groupDo = (doFunc: ((controller: DraggleBarController) => void)) => {
+        const groupId = this.props.group
+        if(groupId){
+            groups.get(groupId)?.forEach(doFunc)
+        }
+    }
+
     @action
     public startDragging = () => {
         this.isDragging = true
+        this.groupDo(controller => {
+            controller.isDragging = true
+        })
     }
 
     @action
     public stopDragging = () => {
         this.isDragging = false
+        this.groupDo(controller => {
+            controller.isDragging = false
+        })
     }
 
     @action
@@ -26,7 +59,7 @@ class DraggleBarController extends baseController<DraggleBarProps>{
         if (this.isDragging) {
             this.props.onDragging?.(e)
         }
-      }
+    }
 }
 
 export default DraggleBarController
--- src/components/draggle-bar/interface.ts
+++ src/components/draggle-bar/interface.ts
@@ -5,5 +5,14 @@ import { ComponentProps } from '../../common/common.interface'
  * 目前只支持竖着的拖动条
  */
 export interface DraggleBarProps extends ComponentProps{
+    /**
+     * 组
+     */
+    group?: string,
+    /**
+     * 当被拖动时
+     * @param e 
+     * @returns 
+     */
     onDragging?: (e) => void
 }
